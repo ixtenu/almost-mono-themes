9d7b7f655c71c4423c6136ed2b1fbedecf8f72a8
ixtenu <ixtenu@pm.me>
2025-01-26 00:26:12 -0800

    Theme tweaks
    - Removed the "cream" variant because I'll never use it
    - Tweaked several color choices
    - Use "highlight" color as "highlight" face
    - Remove bold/italic from font-lock faces
    - Untabify and reindent

--- almost-mono-themes.el
+++ almost-mono-themes.el
@@ -1,6 +1,7 @@
 ;;; almost-mono-themes.el --- Almost monochromatic color themes -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019 - 2022 John Olsson
+;; Copyright (c) 2019-2022 John Olsson
+;; Copyright (c) 2025 ixtenu
 
 ;; Author: John Olsson <john@cryon.se>
 ;; Maintainer: John Olsson <john@cryon.se>
@@ -30,153 +31,141 @@
 ;;; Code:
 
 (defconst almost-mono-themes-colors
-  '((white . ((background . "#ffffff")
-	      (foreground . "#000000")
-	      (weak	  . "#888888")
-	      (weaker	  . "#dddddd")
-	      (weakest    . "#efefef")
-	      (highlight  . "#fda50f")
-	      (warning	  . "#ff0000")
-	      (success	  . "#00ff00")
-	      (string     . "#3c5e2b")))
+  '((white  . ((background . "#ffffff")
+               (foreground . "#000000")
+               (weak       . "#777777")
+               (weaker     . "#dddddd")
+               (weakest    . "#efefef")
+               (highlight  . "#dddddd")
+               (warning    . "#ff0000")
+               (success    . "#4682b4")
+               (string     . "#777777")))
 
-    (black . ((background . "#000000")
-	      (foreground . "#ffffff")
-	      (weak	  . "#aaaaaa")
-	      (weaker	  . "#666666")
-	      (weakest	  . "#222222")
-	      (highlight  . "#fda50f")
-	      (warning	  . "#ff0000")
-	      (success	  . "#00ff00")
-	      (string     . "#a7bca4")))
+    (black .  ((background . "#000000")
+               (foreground . "#ffffff")
+               (weak       . "#aaaaaa")
+               (weaker     . "#666666")
+               (weakest    . "#222222")
+               (highlight  . "#333333")
+               (warning    . "#ff0000")
+               (success    . "#4682b4")
+               (string     . "#aaaaaa")))
 
-    (gray .  ((background . "#2b2b2b")
-	      (foreground . "#ffffff")
-	      (weak	  . "#aaaaaa")
-	      (weaker	  . "#666666")
-	      (weakest	  . "#222222")
-	      (highlight  . "#fda50f")
-	      (warning	  . "#ff0000")
-	      (success	  . "#00ff00")
-	      (string     . "#a7bca4")))
-
-    (cream . ((background . "#f0e5da")
-	      (foreground . "#000000")
-	      (weak	  . "#7d7165")
-	      (weaker	  . "#c4baaf")
-	      (weakest    . "#dbd0c5")
-	      (highlight  . "#fda50f")
-	      (warning	  . "#ff0000")
-	      (success	  . "#00ff00")
-	      (string     . "#3c5e2b")))))
+    (gray  .  ((background . "#2b2b2b")
+               (foreground . "#ffffff")
+               (weak       . "#aaaaaa")
+               (weaker     . "#666666")
+               (weakest    . "#222222")
+               (highlight  . "#444444")
+               (warning    . "#ff0000")
+               (success    . "#4682b4")
+               (string     . "#aaaaaa")))))
 
 (defmacro almost-mono-themes--variant-with-colors (variant &rest body)
   "Execute BODY in a scope where the different colors for given VARIANT is bound."
   `(let* ((colors (or (cdr (assoc ,variant almost-mono-themes-colors))
-		      (error "No such theme variant")))
-	  (background (cdr (assoc 'background colors)))
-	  (foreground (cdr (assoc 'foreground colors)))
-	  (weak	      (cdr (assoc 'weak colors)))
-	  (weaker     (cdr (assoc 'weaker colors)))
-	  (weakest    (cdr (assoc 'weakest colors)))
-	  (highlight  (cdr (assoc 'highlight colors)))
-	  (warning    (cdr (assoc 'warning colors)))
-	  (success    (cdr (assoc 'success colors)))
-	  (string     (cdr (assoc 'string colors))))
+                    (error "No such theme variant")))
+           (background (cdr (assoc 'background colors)))
+           (foreground (cdr (assoc 'foreground colors)))
+           (weak       (cdr (assoc 'weak colors)))
+           (weaker     (cdr (assoc 'weaker colors)))
+           (weakest    (cdr (assoc 'weakest colors)))
+           (highlight  (cdr (assoc 'highlight colors)))
+           (warning    (cdr (assoc 'warning colors)))
+           (success    (cdr (assoc 'success colors)))
+           (string     (cdr (assoc 'string colors))))
      ,@body))
 
 (defmacro almost-mono-themes--faces-spec ()
   "Provide the faces specification."
   (quote
-   (mapcar
-    (lambda (entry) (list (car entry) `((t ,@(cdr entry)))))
-    `(
+    (mapcar
+      (lambda (entry) (list (car entry) `((t ,@(cdr entry)))))
+      `(
+         ;; default
+         (default (:background ,background :foreground ,foreground))
+         (fringe  (:background ,background))
+         (highlight (:background ,highlight))
+         (region  (:background ,highlight  :foreground ,foreground))
+         (show-paren-match (:background ,background :foreground ,success :bold t))
+         (show-paren-mismatch (:background ,background :foreground ,warning :bold t))
+         (minibuffer-prompt (:weight bold :foreground ,foreground))
+         (isearch (:background ,weak :foreground ,foreground :bold t))
+         (lazy-highlight (:background ,weaker :foreground ,foreground))
+         (link (:underline t))
 
-      ;; default
-      (default (:background ,background :foreground ,foreground))
-      (fringe  (:background ,background))
-      (region  (:background ,highlight  :foreground ,foreground))
-      (show-paren-match (:background ,background :foreground ,success :bold t))
-      (show-paren-mismatch (:background ,background :foreground ,warning :bold t))
-      (minibuffer-prompt (:weight bold :foreground ,foreground))
-      (isearch (:background ,weak :foreground ,foreground :bold t))
-      (lazy-highlight (:background ,weaker :foreground ,foreground))
-      (link (:underline t))
+         ;; mode line
+         (mode-line (:box (:line-width -1 :color ,weaker)
+                      :background ,weakest :foreground ,foreground))
+         (mode-line-inactive (:box (:line-width -1 :color ,weaker)
+                               :background ,background :foreground ,weaker))
 
-      ;; mode line
-      (mode-line (:box (:line-width -1 :color ,weaker)
-		       :background ,weakest :foreground ,foreground))
+         ;; font lock
+         (font-lock-keyword-face (:bold nil))
+         (font-lock-function-name-face (:bold nil))
+         (font-lock-variable-name-face (:foreground ,foreground))
+         (font-lock-warning-face (:foreground ,foreground :underline (:color ,warning :style wave)))
+         (font-lock-builtin-face (:bold nil))
+         (font-lock-variable-name-face (:foreground ,foreground :italic nil))
+         (font-lock-constant-face (:bold nil :italic nil))
+         (font-lock-type-face (:italic nil))
+         (font-lock-preprocessor-face (:italic nil))
+         (font-lock-comment-face (:foreground ,weak :italic nil))
+         (font-lock-string-face (:foreground ,string))
+         (font-lock-doc-face (:inherit font-lock-comment-face))
+         (line-number (:foreground ,weaker))
+         (linum (:inherit line-number))
+         (vertical-border (:foreground ,weaker))
 
-      (mode-line-inactive (:box (:line-width -1 :color ,weaker)
-				:background ,background :foreground ,weaker))
+         ;; eshell
+         (eshell-prompt (:foreground ,foreground :bold t))
+         (eshell-ls-directory (:foreground ,foreground :bold t))
+         (eshell-ls-archive (:inherit eshell-ls-unreadable))
+         (eshell-ls-backup (:inherit eshell-ls-unreadable))
+         (eshell-ls-clutter (:inherit eshell-ls-unreadable))
+         (eshell-ls-executable (:inherit eshell-ls-unreadable))
+         (eshell-ls-missing (:inherit eshell-ls-unreadable))
+         (eshell-ls-product (:inherit eshell-ls-unreadable))
+         (eshell-ls-readonly (:inherit eshell-ls-unreadable))
+         (eshell-ls-special (:inherit eshell-ls-unreadable))
+         (eshell-ls-symlink (:inherit eshell-ls-unreadable))
 
-      ;; font lock
-      (font-lock-keyword-face (:bold t))
-      (font-lock-function-name-face (:bold t))
-      (font-lock-variable-name-face (:foreground ,foreground))
-      (font-lock-warning-face (:foreground ,foreground :underline (:color ,warning :style wave)))
-      (font-lock-builtin-face (:bold t))
-      (font-lock-variable-name-face (:foreground ,foreground :italic t))
-      (font-lock-constant-face (:bold t :italic t))
-      (font-lock-type-face (:italic t))
-      (font-lock-preprocessor-face (:italic t))
-      (font-lock-comment-face (:foreground ,weak :italic t))
-      (font-lock-string-face (:foreground ,string))
-      (font-lock-doc-face (:inherit font-lock-comment-face))
-      (line-number (:foreground ,weaker))
-      (linum (:inherit line-number))
-      (vertical-border (:foreground ,weaker))
+         ;; company mode
+         (company-tooltip (:background ,weakest :foreground ,foreground))
+         (company-tooltip-selection (:background ,weaker :foreground ,foreground))
+         ;;(company-tooltip-search (:background "#ff0000" :foreground "#00ff00"))
+         (company-tooltip-common (:bold t))
+         (company-tooltip-common-selection (:bold t))
+         (company-scrollbar-bg (:background ,weaker))
+         (company-scrollbar-fg (:background ,weak))
+         (company-tooltip-annotation-selection (:background ,weaker :foreground ,foreground :italic t))
+         (company-tooltip-annotation (:background ,weakest :foreground ,weak :italic t))
 
-      ;; eshell
-      (eshell-prompt (:foreground ,foreground :bold t))
-      (eshell-ls-directory (:foreground ,foreground :bold t))
-      (eshell-ls-archive (:inherit eshell-ls-unreadable))
-      (eshell-ls-backup (:inherit eshell-ls-unreadable))
-      (eshell-ls-clutter (:inherit eshell-ls-unreadable))
-      (eshell-ls-executable (:inherit eshell-ls-unreadable))
-      (eshell-ls-missing (:inherit eshell-ls-unreadable))
-      (eshell-ls-product (:inherit eshell-ls-unreadable))
-      (eshell-ls-readonly (:inherit eshell-ls-unreadable))
-      (eshell-ls-special (:inherit eshell-ls-unreadable))
-      (eshell-ls-symlink (:inherit eshell-ls-unreadable))
+         ;; git gutter
+         (git-gutter:modified (:background ,highlight :foreground ,highlight))
+         (git-gutter:added (:background ,success :foreground ,success))
+         (git-gutter:deleted (:background ,warning :foreground ,warning))
 
-      ;; company mode
-      (company-tooltip (:background ,weakest :foreground ,foreground))
-      (company-tooltip-selection (:background ,weaker :foreground ,foreground))
-      ;;(company-tooltip-search (:background "#ff0000" :foreground "#00ff00"))
-      (company-tooltip-common (:bold t))
-      (company-tooltip-common-selection (:bold t))
-      (company-scrollbar-bg (:background ,weaker))
-      (company-scrollbar-fg (:background ,weak))
-      (company-tooltip-annotation-selection (:background ,weaker :foreground ,foreground :italic t))
-      (company-tooltip-annotation (:background ,weakest :foreground ,weak :italic t))
+         ;; diff hl
+         (diff-hl-change (:background ,highlight :foreground ,highlight))
+         (diff-hl-insert (:background ,success :foreground ,success))
+         (diff-hl-delete (:background ,warning :foreground ,warning))
 
-      ;; git gutter
-      (git-gutter:modified (:background ,highlight :foreground ,highlight))
-      (git-gutter:added (:background ,success :foreground ,success))
-      (git-gutter:deleted (:background ,warning :foreground ,warning))
+         ;; hl line
+         (hl-line (:background ,weakest))
+         (highlight-current-line-face (:inherit hl-line))
 
-      ;; diff hl
-      (diff-hl-change (:background ,highlight :foreground ,highlight))
-      (diff-hl-insert (:background ,success :foreground ,success))
-      (diff-hl-delete (:background ,warning :foreground ,warning))
+         ;; ido
+         (ido-first-match (:bold t))
+         (ido-only-match (:bold t))
+         (ido-subdir (:italic t))
+         (ido-virtual (:foreground ,weak))
+         (ido-vertical-match-face (:bold t :italic nil))
 
-      ;; hl line
-      (hl-line (:background ,weakest))
-      (highlight-current-line-face (:inherit hl-line))
+         ;; org mode
+         (org-table (:foreground ,weak))
 
-      ;; ido
-      (ido-first-match (:bold t))
-      (ido-only-match (:bold t))
-      (ido-subdir (:italic t))
-      (ido-virtual (:foreground ,weak))
-      (ido-vertical-match-face (:bold t :italic nil))
-
-      ;; org mode
-      (org-table (:foreground ,weak))
-
-      ))))
-
+         ))))
 
 (defun almost-mono-themes--variant-name (variant)
   "Create symbol for color theme variant VARIANT."
@@ -185,20 +174,20 @@
 (defmacro almost-mono-themes--define-theme (variant)
   "Define a theme for the almost-mono variant VARIANT."
   (let ((name (almost-mono-themes--variant-name variant))
-        (doc (format "almost mono theme (%s version)" variant)))
+         (doc (format "almost mono theme (%s version)" variant)))
     `(progn
        (deftheme ,name ,doc)
        (put ',name 'theme-immediate t)
        (almost-mono-themes--variant-with-colors
-        ',variant
-        (apply 'custom-theme-set-faces ',name
-               (almost-mono-themes--faces-spec)))
+         ',variant
+         (apply 'custom-theme-set-faces ',name
+           (almost-mono-themes--faces-spec)))
        (provide-theme ',name))))
 
 ;;;###autoload
 (when (and (boundp 'custom-theme-load-path) load-file-name)
   (add-to-list 'custom-theme-load-path
-	       (file-name-as-directory (file-name-directory load-file-name))))
+    (file-name-as-directory (file-name-directory load-file-name))))
 
 (provide 'almost-mono-themes)
 
